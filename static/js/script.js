4d0929b0e466760dcafdc4add56531a74676eaae
James McBeath <jamesmcbeath@me.com>
2023-06-05 09:47:44 +0000

    Add dropdown JQuery

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,10 @@
+// Initializes the side navigation menu with the option to position it on the right side.
 $(document).ready(function(){
   $('.sidenav').sidenav({edge: "right"});
 });
+// Accordion function
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+// Dropdown trigger and structure function
+$('.dropdown-trigger').dropdown();
